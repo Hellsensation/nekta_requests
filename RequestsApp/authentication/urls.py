7d87e870530afcde76a94cfa09603c9311f82335
hellsensation <hellsensation@gmail.com>
2023-11-19 19:39:42 +0400

    замена на LoginView.as_view

--- RequestsApp/authentication/urls.py
+++ RequestsApp/authentication/urls.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
-from .views import login_view
+#from .views import login_view
 
 app_name = 'authentication'
 
 
 urlpatterns = [
-    path('login/', login_view, name='login')
+    #path('login/', login_view, name='login'),
+    path('login/', LoginView.as_view(template_name='authentication/login.html'), name='login')
+
 ]
